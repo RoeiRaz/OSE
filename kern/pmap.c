ca1b5534714f32b401ad088b2f054a74d94e492e
Roei <m.roei.raz@gmail.com>
2017-04-23 11:16:51 +0300

    allocate envs - boot_alloc, create UENV image

--- kern/pmap.c
+++ kern/pmap.c
@@ -164,7 +164,10 @@ mem_init(void)
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
-
+    envs = (struct Env *) boot_alloc(NENV * sizeof(*envs));
+    memset(envs, 0, NENV * sizeof(*envs));
+    
+    
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
 	// up the list of free physical pages. Once we've done so, all further
@@ -202,6 +205,13 @@ mem_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
+    
+    boot_map_region(kern_pgdir,
+                    (intptr_t) UENVS,
+                    PTSIZE,
+                    PADDR(envs),
+                    PTE_U
+    );
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
