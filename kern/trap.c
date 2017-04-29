6e6e2944d85871b70e95a015ddc094386ddb5c53
RoeiRaz <m.roei.raz@gmail.com>
2017-04-29 13:07:21 +0300

    exercise 5, added page_fault_handler() call

--- kern/trap.c
+++ kern/trap.c
@@ -158,7 +158,11 @@ trap_dispatch(struct Trapframe *tf)
 {
 	// Handle processor exceptions.
 	// LAB 3: Your code here.
-
+	switch (tf->tf_trapno) {
+		case T_PGFLT:
+			page_fault_handler(tf);
+			break;
+	}
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
 	if (tf->tf_cs == GD_KT)
