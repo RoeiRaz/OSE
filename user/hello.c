437f55d8e5f07c2c36af8e6943783ce4a10b57c5
Yosef Raisman <yosefraisman@gmail.com>
2017-05-16 12:42:11 +0300

    Comment Out Test

--- user/hello.c
+++ user/hello.c
@@ -6,18 +6,22 @@ umain(int argc, char **argv)
 {
 	cprintf("hello, world\n");
 	cprintf("i am environment %08x\n", thisenv->env_id);
+	
+	/*
+	int pid, i;
 
-	int pid;
-
-	for (int i = 0; i < 20; i++) {
+	for (i = 0; i < 20; i++) {
 		if ((pid = priority_fork(20-i)) == 0) {
-			cprintf("start----priority %d\n", 20 - i);
-			for (int j = 0; j < 5; j++) {
-				cprintf("yield----priority %d\n", 20 - i);
-				sys_yield();
-			}
 			break;
 		}
 	}
+
+	if(pid == 0) {
+		cprintf("start----priority %d\n", 20 - i);
+		for (int j = 0; j < 5; j++) {
+			cprintf("yield----priority %d\n", 20 - i);
+			sys_yield();
+		}
+	} */
 
 }
