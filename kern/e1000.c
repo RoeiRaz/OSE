605a7857780989e42163fd3641223ffa946b9562
RoeiRaz <m.roei.raz@gmail.com>
2017-05-26 16:56:30 +0300

    ex3

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,3 +1,9 @@
 #include <kern/e1000.h>
+#include <kern/pci.h>
 
 // LAB 6: Your driver code here
+int e1000_attachfn(struct pci_func *pcif) {
+    // Currently, we only enable the device without doing anything else.
+    pci_func_enable(pcif);
+    return 0;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,4 +1,20 @@
+/* e1000.h - e1000 slim kernel, 26/5/2017, Roei Rosenzweig and Yosef Raisman © */
+
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
+
+#include <kern/pci.h>
+
+/**
+ * PCI attach function
+ * TODO maybe we should rename this to something nicer
+ */
+int e1000_attachfn(struct pci_func *pcif);
+
+/**
+ * Device constants
+ */
+#define E1000_VENDOR_ID         (0x8086) // noice
+#define E1000_PRODUCT_ID        (0x100e) // this is really cool
 
 #endif	// JOS_KERN_E1000_H
